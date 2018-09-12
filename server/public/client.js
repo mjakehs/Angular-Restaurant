c911a146daf9c63139db90bab9315eb7e61bcfd3
Jake Marzitelli <mjakehs@gmail.com>
2018-09-12 14:30:47 -0500

    added delicous field, added input validator and reseter

--- server/public/client.js
+++ server/public/client.js
@@ -1,10 +1,22 @@
 const foodApp = angular.module('FoodApp', []);
 
-foodApp.controller('FoodController', function (){
+foodApp.controller('FoodController', function () {
     let self = this;
-    self.foods = ['Baked Beans', 'Chocolate Fishsticks', 'Soft Boiled Egg'];
+    self.foods = [{ name: 'Baked Beans', deliciousness: 50 }, { name: 'Chocolate Fishsticks', deliciousness: 95 }, { name: 'Soft Boiled Egg', deliciousness: 66 }];
     self.inputFood = '';
-    self.addFood = function(foodName) {
-        self.foods.push(foodName);
+    self.inputDelicous = 0;
+    self.addFood = function (foodName, delicious) {
+        if (Number(delicious) > 0 && Number(delicious) <= 100) {
+            self.foods.push({
+                name: foodName,
+                deliciousness: delicious
+            }
+            );
+        }
+        else {
+            alert('Please enter inputs in correct format!');
+            self.inputFood = '';
+            self.inputDelicious = '';
+        }
     }
 })
