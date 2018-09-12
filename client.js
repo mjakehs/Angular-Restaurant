7b83fc74abe0ed5b454d4147d69917591dbc9836
Jake Marzitelli <mjakehs@gmail.com>
2018-09-12 14:03:10 -0500

    create foods, add food to list on DOM

--- client.js
+++ client.js
@@ -2,6 +2,7 @@ const foodApp = angular.module('FoodApp', []);
 
 foodApp.controller('FoodController', function (){
     let self = this;
+    self.foods = ['Baked Beans', 'Chocolate Fishsticks', 'Soft Boiled Egg'];
     self.greet = function() {
         console.log('Angular Ready');
     }
