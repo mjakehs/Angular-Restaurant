f1f7b56643af315de6a390048929d89f219a597f
Jake Marzitelli <mjakehs@gmail.com>
2018-09-12 14:09:32 -0500

    add user ability to push a new food name into the list

--- client.js
+++ client.js
@@ -3,7 +3,8 @@ const foodApp = angular.module('FoodApp', []);
 foodApp.controller('FoodController', function (){
     let self = this;
     self.foods = ['Baked Beans', 'Chocolate Fishsticks', 'Soft Boiled Egg'];
-    self.greet = function() {
-        console.log('Angular Ready');
+    self.inputFood = '';
+    self.addFood = function(foodName) {
+        self.foods.push(foodName);
     }
 })
